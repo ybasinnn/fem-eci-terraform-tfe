dc6d115e429c0e2593718f9eb8b7c389dc4f2a64
Yoda <basinyordan@gmail.com>
2024-09-27 17:28:23 +0300

    setup github workspace

--- locals.tf
+++ locals.tf
@@ -9,8 +9,14 @@ locals {
       description         = "Example description of workspace"
       execution_mode      = "remote"
       project_id          = module.project["fem-eci-project"].id
-      vcs_repo_identifier = "ybasinnn/fem-eci-terraform-tfe"
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
 
+    }
+    "fem-eci-github" = {
+      description = "Example automation workspace for Github resources"
+      execution_mode = "local"
+      project_id = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
