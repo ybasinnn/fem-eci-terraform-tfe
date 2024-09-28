32902264fce59516bfd71e2ed4df830a734e8c48
Yoda <basinyordan@gmail.com>
2024-09-28 17:16:48 +0300

    feat: added workspaces

--- locals.tf
+++ locals.tf
@@ -18,5 +18,61 @@ locals {
       project_id = module.project["fem-eci-project"].id
       vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
+    
+    "fem-eci-aws-network" = {
+      description         = "Automation for AWS network resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-network"
+
+      variables = [
+        {
+          category = "terraform"
+          hcl      = true
+          key      = "azs"
+          value    = jsonencode(["us-west-2a", "us-west-2b"])
+        },
+        {
+          category = "terraform"
+          key      = "cidr"
+          value    = "10.0.0.0/16"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci"
+        },
+      ]
+    }
+
+    "fem-eci-aws-cluster-prod" = {
+      description         = "Automation for AWS cluster resources."
+      execution_mode      = "remote"
+      project_id          = module.project["fem-eci-project"].id
+      vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-aws-cluster"
+
+      variables = [
+        {
+          category = "terraform"
+          key      = "domain"
+          value    = "danko123.ninja"
+        },
+        {
+          category = "terraform"
+          key      = "environment"
+          value    = "prod"
+        },
+        {
+          category = "terraform"
+          key      = "name"
+          value    = "fem-eci-ybasinnnn"
+        },
+        {
+          category = "terraform"
+          key      = "vpc_name"
+          value    = "fem-eci"
+        },
+      ]
+    }
   }
 }
